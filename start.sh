2a547355bc10fcdb0b5f24ba6d9e0323c4269342
Ulisses Caon <ulissescaon@gmail.com>
2020-03-22 16:01:28 -0300

    Initialize docker structure

---
+++ start.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+# Adapted from Alex Kleissner's post, Running a Phoenix 1.3 project with docker-compose
+# https://medium.com/@hex337/running-a-phoenix-1-3-project-with-docker-compose-d82ab55e43cf
+set -e
+# shellcheck disable=SC2039
+source ./.env
+
+# Ensure the app's dependencies are installed
+mix deps.get
+
+# Install JS libraries
+echo "Installing JS..."
+cd apps/plutus_web/assets && npm install
+cd ../../..
+
+# Wait for Postgres to become available.
+until psql "postgresql://$POSTGRES_USER:$POSTGRES_PASSWORD@database/$POSTGRES_DATABASE" '\q' 2>/dev/null; do
+  >&2 echo "Postgres is unavailable - retrying in 1 second"
+  sleep 1
+done
+
+# Potentially Set up the database
+mix ecto.create
+mix ecto.migrate
+
+echo "Launching Phoenix web server..."
+# Start the phoenix web server
+mix phx.server
