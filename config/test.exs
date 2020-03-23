17eefc1d0179bc91920f36e357c0e2fdf73275c6
Ulisses Caon <ulissescaon@gmail.com>
2020-03-23 14:41:55 -0300

    Set up test database

--- config/test.exs
+++ config/test.exs
@@ -5,7 +5,7 @@ config :plutus, Plutus.Repo,
   username: "postgres",
   password: "postgres",
   database: "plutus_test",
-  hostname: "localhost",
+  hostname: "database",
   pool: Ecto.Adapters.SQL.Sandbox
 
 # We don't run a server during test. If one is required,
