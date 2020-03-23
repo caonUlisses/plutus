107e240e9157689887cbe30114fb2b92225f8dc4
Ulisses Caon <ulissescaon@gmail.com>
2020-03-23 19:12:16 -0300

    Update documentation url

--- lib/plutus_web/controllers/index_controller.ex
+++ lib/plutus_web/controllers/index_controller.ex
@@ -2,7 +2,7 @@ defmodule PlutusWeb.IndexController do
   use PlutusWeb, :controller
 
   def index(conn, _params) do
-    conn |> json(%{message: "Visit the docs at: "})
+    conn |> json(%{message: "Visit the docs at: plutus4.docs.apiary.io"})
   end
 
   def reports_url(conn, _params) do
