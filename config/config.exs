d4aa25cc4cab425d129d0bbeef5316a81a8632c3
Ulisses Caon <ulissescaon@gmail.com>
2020-03-23 14:41:40 -0300

    Configure guardian and format file

--- config/config.exs
+++ config/config.exs
@@ -8,19 +8,29 @@
 use Mix.Config
 
 config :plutus,
-  ecto_repos: [Plutus.Repo]
+       ecto_repos: [Plutus.Repo]
 
 # Configures the endpoint
-config :plutus, PlutusWeb.Endpoint,
-  url: [host: "localhost"],
-  secret_key_base: "YUTtgCQAJLCEe4qlgTpsTR8sZ8hhfuZnea+4PRiT3knIGd6nFEL7C5b/RcpZbe5I",
-  render_errors: [view: PlutusWeb.ErrorView, accepts: ~w(json)],
-  pubsub: [name: Plutus.PubSub, adapter: Phoenix.PubSub.PG2]
+config :plutus,
+       PlutusWeb.Endpoint,
+       url: [
+         host: "localhost"
+       ],
+       secret_key_base: "YUTtgCQAJLCEe4qlgTpsTR8sZ8hhfuZnea+4PRiT3knIGd6nFEL7C5b/RcpZbe5I",
+       render_errors: [
+         view: PlutusWeb.ErrorView,
+         accepts: ~w(json)
+       ],
+       pubsub: [
+         name: Plutus.PubSub,
+         adapter: Phoenix.PubSub.PG2
+       ]
 
 # Configures Elixir's Logger
-config :logger, :console,
-  format: "$time $metadata[$level] $message\n",
-  metadata: [:request_id]
+config :logger,
+       :console,
+       format: "$time $metadata[$level] $message\n",
+       metadata: [:request_id]
 
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
@@ -28,3 +38,8 @@ config :phoenix, :json_library, Jason
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env()}.exs"
+
+config :plutus,
+       Plutus.Guardian,
+       issuer: "plutus",
+       secret_key: "Sqo9zNbKNv0aRLV9yzogveQTRc87IgDukW0XYDGErs8tSDWn+J3aInjnZ65DbOBk"
